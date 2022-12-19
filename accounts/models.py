0f2399b08857ff7ed236ec0f906105a87e01c09a
mohammad330a <m.a.alizadeh330@gmail.com>
2022-12-19 21:20:19 +0330

    add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    # fields will be added later
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='created at'
+    )
+
+    def __str__(self):
+        return f"{self.pk}:) {self.username}"
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
