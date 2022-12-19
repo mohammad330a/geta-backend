74b97ee4b24eb029ddc8fe22429aba47d085ca01
ErfanMoradi2000 <79erfanmoradi@gmail.com>
2022-12-19 05:24:50 +0330

    creted field was added to Offer and Course tables

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=50)  # it could be primary key as well(it helps the proces of )
+    name = models.CharField(max_length=50)  # it could be primary key as well
     campus = models.CharField(max_length=50)
 
     def __str__(self):
@@ -16,6 +16,7 @@ class Request(models.Model):
     telegram_id = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField()
     description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField()
 
     def __str__(self):
         return self.course
@@ -27,6 +28,7 @@ class Offer(models.Model):
     telegram_id = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField()
     description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField()
 
     def __str__(self):
         return self.course
