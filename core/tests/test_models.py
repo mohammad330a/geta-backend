acab9e03d640bc585892865928b74d2d9478d0a0
MGiniio <saleh.shakour2000@gmail.com>
2022-12-20 20:34:27 +0330

    testing Request model

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from ..models import Course,Request,Offer
+from accounts.models import User
+from datetime import datetime
+
 
 class ModelTest(TestCase):
     def testCourseModel(self):
@@ -7,3 +10,13 @@ class ModelTest(TestCase):
         self.assertEquals(str(course), 'AdvanceProgramming')
         print("IsInstance : ",isinstance(course,Course))
         self.assertTrue(isinstance(course,Course))
+    
+    def testRequestModel(self):
+        course = Course.objects.create(name="AdvanceProgramming")
+        user = User.objects.create()
+        request = Request.objects.create(student= user, course= course, email="saleh@gmail.com", created= datetime.now())
+        self.assertEquals((str(request.course)), 'AdvanceProgramming')
+        self.assertEquals(str(request.email), 'saleh@gmail.com')
+        self.assertTrue(isinstance(request,Request))
+        self.assertNotEqual(request.created,datetime.now())
+        
