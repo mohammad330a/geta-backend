81622e3aa8f746390672369da476bde579435f53
ErfanMoradi2000 <79erfanmoradi@gmail.com>
2022-12-28 04:39:45 +0330

    the crud APIs is created

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from .views import RequestList, RequestDetail, OfferList, OfferDetail
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-
+    path('Request/', RequestList.as_view()),
+    path('Request/<int:pk>/', RequestDetail.as_view()),
+    path('Offer/', OfferList.as_view()),
+    path('Offer/<int:pk>/', OfferDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
