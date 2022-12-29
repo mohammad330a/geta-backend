e8e34216d73dfd02b5c091af9d2f53b2ad342f73
mohammad330a <m.a.alizadeh330@gmail.com>
2022-12-29 18:52:47 +0330

    change url to lowercase letters

--- core/urls.py
+++ core/urls.py
@@ -4,16 +4,16 @@ from .views import RequestList, RequestCreate, RequestRetrieve, RequestUpdate, R
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('Request/', RequestList.as_view()),
-    path('Request/create/', RequestCreate.as_view()),
-    path('Request/<int:pk>/', RequestRetrieve.as_view()),
-    path('Request/update/<int:pk>/', RequestUpdate.as_view()),
-    path('Request/destroy/<int:pk>/', RequestDestroy.as_view()),
-    path('Offer/', OfferList.as_view()),
-    path('Offer/create/', OfferCreate.as_view()),
-    path('Offer/<int:pk>/', OfferRetrieve.as_view()),
-    path('Offer/update/<int:pk>/', OfferUpdate.as_view()),
-    path('Offer/destroy/<int:pk>/', OfferDestroy.as_view()),
+    path('request/', RequestList.as_view()),
+    path('request/create/', RequestCreate.as_view()),
+    path('request/<int:pk>/', RequestRetrieve.as_view()),
+    path('request/update/<int:pk>/', RequestUpdate.as_view()),
+    path('request/destroy/<int:pk>/', RequestDestroy.as_view()),
+    path('offer/', OfferList.as_view()),
+    path('offer/create/', OfferCreate.as_view()),
+    path('offer/<int:pk>/', OfferRetrieve.as_view()),
+    path('offer/update/<int:pk>/', OfferUpdate.as_view()),
+    path('offer/destroy/<int:pk>/', OfferDestroy.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
