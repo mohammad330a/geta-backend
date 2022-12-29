6d696f7ccb1cf53dfdb7b1cba5cf1a41777c148d
ErfanMoradi2000 <79erfanmoradi@gmail.com>
2022-12-29 12:11:44 +0330

    the CRUD operation is seprately designed

--- core/views.py
+++ core/views.py
@@ -1,26 +1,66 @@
 from .models import Request, Offer
 from .serializers import RequestSerializer, OfferSerializer
 from rest_framework import generics, permissions
+from django.shortcuts import get_object_or_404
+from accounts.models import User
 
 
-class RequestList(generics.ListCreateAPIView):
+
+class RequestList(generics.ListAPIView):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
 
 
-class RequestDetail(generics.RetrieveUpdateDestroyAPIView):
+class RequestCreate(generics.CreateAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    # def perform_create(self, serializer):
+    #     student = get_object_or_404(User, id=self.request.data.get('username'))
+    #     return serializer.save(student=student)
+
+
+class RequestRetrieve(generics.RetrieveAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+
+
+class RequestUpdate(generics.UpdateAPIView):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
     permission_classes = (permissions.IsAuthenticated, )
 
 
-class OfferList(generics.ListCreateAPIView):
+class RequestDestroy(generics.DestroyAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class OfferList(generics.ListAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
 
 
-class OfferDetail(generics.RetrieveUpdateDestroyAPIView):
+class OfferCreate(generics.CreateAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
     permission_classes = (permissions.IsAuthenticated, )
 
+
+class OfferRetrieve(generics.RetrieveAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+
+
+class OfferUpdate(generics.UpdateAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+
+class OfferDestroy(generics.DestroyAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = (permissions.IsAuthenticated, )
