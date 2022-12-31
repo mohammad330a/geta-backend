af0c75122b5a4e05781bff6fd7c83b2cfca7f8dc
MGiniio <saleh.shakour2000@gmail.com>
2022-12-31 22:58:28 +0330

    write a test for RequestCreate

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -6,11 +6,12 @@ from ..views import *
 class UrlTest(TestCase):
     def testRequestList(self):
         url = reverse('RequestList')
-        print(url)
         self.assertEquals(resolve(url).func.view_class, RequestList)
         
     def testRequestList(self):
         url = reverse('UserRequestList')
-        print(url)
         self.assertEquals(resolve(url).func.view_class, UserRequestList)
 
+    def testRequestList(self):
+        url = reverse('RequestCreate')
+        self.assertEquals(resolve(url).func.view_class, RequestCreate)
