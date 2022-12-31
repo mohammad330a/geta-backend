d27ea429d683b5500370402dac9b6a3d0a788892
mohammad330a <m.a.alizadeh330@gmail.com>
2022-12-31 17:35:01 +0330

    handle cors headers

--- software_design_backend/settings.py
+++ software_design_backend/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'dj_rest_auth',
     'core.apps.CoreConfig',
     'accounts.apps.AccountsConfig',
@@ -55,11 +56,17 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+
+CORS_ALLOWED_ORIGINS = [
 ]
 
 ROOT_URLCONF = 'software_design_backend.urls'
