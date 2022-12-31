32661acfaffa69b27df0651c21e42c2acf7389a0
MGiniio <saleh.shakour2000@gmail.com>
2022-12-31 22:44:47 +0330

    Write tests for RequestList

---
+++ core/tests/test_urls.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from ..views import *
+
+
+class UrlTest(TestCase):
+    def testRequestList(self):
+        url = reverse('RequestList')
+        print(url)
+        self.assertEquals(resolve(url).func.view_class, RequestList)
+
