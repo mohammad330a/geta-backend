1d780b222099aa0f1770dbc60363c9e2b6069e9f
MGiniio <saleh.shakour2000@gmail.com>
2022-12-31 22:35:56 +0330

    name urls for test

--- core/urls.py
+++ core/urls.py
@@ -18,20 +18,20 @@ from .views import (
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('request/', RequestList.as_view()),
-    path('request/user/', UserRequestList.as_view()),
-    path('request/create/', RequestCreate.as_view()),
-    path('request/<int:pk>/', RequestRetrieve.as_view()),
-    path('request/update/<int:pk>/', RequestUpdate.as_view()),
-    path('request/destroy/<int:pk>/', RequestDestroy.as_view()),
-    path('offer/', OfferList.as_view()),
-    path('offer/user/', UserOfferList.as_view()),
-    path('offer/create/', OfferCreate.as_view()),
-    path('offer/<int:pk>/', OfferRetrieve.as_view()),
-    path('offer/update/<int:pk>/', OfferUpdate.as_view()),
-    path('offer/destroy/<int:pk>/', OfferDestroy.as_view()),
-    path('course/', CourseList.as_view()),
-    path('course/<int:pk>/', CourseRetrieve.as_view()),
+    path('request/', RequestList.as_view(), name='RequestList'),
+    path('request/user/', UserRequestList.as_view(), name='UserRequestList'),
+    path('request/create/', RequestCreate.as_view(), name='RequestCreate'),
+    path('request/<int:pk>/', RequestRetrieve.as_view(), name='RequestRetrieve'),
+    path('request/update/<int:pk>/', RequestUpdate.as_view(), name='RequestUpdate'),
+    path('request/destroy/<int:pk>/', RequestDestroy.as_view(), name='RequestDestroy'),
+    path('offer/', OfferList.as_view(), name='OfferList'),
+    path('offer/user/', UserOfferList.as_view(), name='UserOfferList'),
+    path('offer/create/', OfferCreate.as_view(), name='OfferCreate'),
+    path('offer/<int:pk>/', OfferRetrieve.as_view(), name='OfferRetrieve'),
+    path('offer/update/<int:pk>/', OfferUpdate.as_view(), name='OfferUpdate'),
+    path('offer/destroy/<int:pk>/', OfferDestroy.as_view(), name='OfferDestroy'),
+    path('course/', CourseList.as_view(),'CourseList'),
+    path('course/<int:pk>/', CourseRetrieve.as_view(),'CourseRetrieve'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
